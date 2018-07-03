39b296d2673e94cdc35605b2883ec331f3883ba0
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-07-03 14:49:03 +0200

    bzr-mover: add support for LAUNCHPAD_OWNER env variable

--- bzr-mover.sh
+++ bzr-mover.sh
@@ -24,6 +24,7 @@ trap cleanup_dirs ERR EXIT SIGQUIT SIGTERM SIGINT
 
 source="$project"
 bzr_repo=$project
+launchpad_owner="ubuntu-desktop"
 
 if [ -n "${ubuntu_aliases[$project]}" ]; then
   source="${ubuntu_aliases[$project]}"
@@ -33,9 +34,15 @@ if [ -n "${ubuntu_bzr_aliases[$project]}" ]; then
   bzr_repo="${ubuntu_bzr_aliases[$project]}"
 fi
 
+if [ -n "$LAUNCHPAD_OWNER" ]; then
+  launchpad_owner="$LAUNCHPAD_OWNER"
+fi
+
 set -xe
 
-bzr_uri="lp:~ubuntu-desktop/$bzr_repo/ubuntu${ubuntu_bzr_sufix_aliases[$project]}"
+bzr_project_repo="$bzr_repo/ubuntu${ubuntu_bzr_sufix_aliases[$project]}"
+bzr_uri="lp:~ubuntu-desktop/$bzr_project_repo"
+bzr_push_uri="lp:~$launchpad_owner/$bzr_project_repo"
 
 bzr branch "$bzr_uri" "$source.bzr"
 cd "$source.bzr" || exit 1
@@ -46,4 +53,4 @@ echo "https://git.launchpad.net/~ubuntu-desktop/ubuntu/+source/$source" >> THIS_
 bzr add THIS_REPOSITORY_HAS_BEEN_MOVED_TO_GIT
 
 bzr commit -m "Moved to git: lp:~ubuntu-desktop/ubuntu/+source/$source"
-bzr push "$bzr_uri"
+bzr push "$bzr_push_uri"
